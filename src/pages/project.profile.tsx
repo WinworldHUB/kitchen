4b8ab0476979d433ec9be75d942690ce2c213d69
Amit Dubey <iamsaagar@yahoo.com>
2024-07-05 12:42:38 +0100

    Updated

--- src/pages/project.profile.tsx
+++ src/pages/project.profile.tsx
@@ -2,7 +2,6 @@ import { useContext, useEffect, useState } from "react";
 import PageLayout from "../lib/components/app.layout";
 import { AppContext } from "../lib/contexts/appcontext";
 import { getProjectById } from "../lib/utils/project.utils";
-import { ProjectStatus } from "../lib/constants";
 import { Col, Row } from "react-bootstrap";
 import CreateProjectContainer from "../lib/components/create.project/create.project.container";
 
@@ -19,15 +18,11 @@ const ProjectProfilePage = () => {
 
   return (
     <PageLayout>
-      {project?.status === ProjectStatus.draft ? (
-        <Row className="justify-content-center">
-          <Col sm="8">
-            <CreateProjectContainer project={project} onCreate={() => {}} />
-          </Col>
-        </Row>
-      ) : (
-        appState.selectedProjectId
-      )}
+      <Row className="justify-content-center">
+        <Col sm="8">
+          <CreateProjectContainer project={project} onCreate={() => {}} />
+        </Col>
+      </Row>
     </PageLayout>
   );
 };
