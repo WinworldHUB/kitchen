a30e0a825cd2bde0942e762099d03ed276c2c0d1
Amit Dubey <iamsaagar@yahoo.com>
2024-08-12 22:42:49 +0100

    Resolved build issues

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,78 +1,74 @@
 import { Button, Col, Form, Row } from "react-bootstrap";
 import PageLayout from "../lib/components/app.layout";
 import CardSimple from "../lib/components/card.simple";
-import { Link, useNavigate } from "react-router-dom";
-import {
-  DEFAULT_LOCAL_STORAGE_KEY_FOR_USER_STATE,
-  PageRoutes,
-} from "../lib/constants";
-import useAuthentication from "../lib/hooks/useAuthentication";
-import { useContext, useState } from "react";
-import { USER_APIS } from "../lib/constants/api-constants";
-import { AppContext } from "../lib/contexts/appcontext";
-import useApi from "../lib/hooks/useApi";
-import useLocalStorage from "../lib/hooks/useLocalStorage";
+import { Link } from "react-router-dom";
+import { PageRoutes } from "../lib/constants";
 import { Formik } from "formik";
 import FormFieldError from "../lib/components/form.field.error";
 import { SIGN_IN_VALIDATION_SCHEME } from "../lib/constants/validation-constants";
 
+const DEFAULT_LOGIN_VALUES: LoginRequest = {
+  email: "",
+  password: "",
+};
+
 const SignInPage = () => {
-  const navigate = useNavigate();
-  const { postData: sendSignUpData } = useApi<SignUpResponse>();
-  const { signInUser } = useAuthentication();
-  const { updateAppState } = useContext(AppContext);
-  const { setValue: setUserState } = useLocalStorage<User>();
+  // const navigate = useNavigate();
+  // const { postData: sendSignUpData } = useApi<SignUpResponse>();
+  // const { signInUser } = useAuthentication();
+  // const { updateAppState } = useContext(AppContext);
+  // const { setValue: setUserState } = useLocalStorage<User>();
   // State for form data
-  const [formData, setFormData] = useState<LoginRequest>({
-    email: "",
-    password: "",
-  });
+  // const [formData, setFormData] = useState<LoginRequest>({
+  //   email: "",
+  //   password: "",
+  // });
 
   // Handle form input change
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { name, value } = event.target;
+  //   setFormData((prevState) => ({
+  //     ...prevState,
+  //     [name]: value,
+  //   }));
+  // };
 
   // Handle form submission
-  const onSignIn = async () => {
-    try {
-      const response = await sendSignUpData(USER_APIS.LOGIN_USER_API, formData);
+  // const onSignIn = async () => {
+  //   try {
+  //     const response = await sendSignUpData(USER_APIS.LOGIN_USER_API, formData);
 
-      // Handle successful response
-      if (response.success) {
-        signInUser({
-          email: formData.email,
-          user_id: response.user_id,
-          sessionToken: response.session_token,
-          sessionJwt: response.session_jwt,
-        });
-        updateAppState({
-          isUserLoggedIn: true,
-          user_id: response.user_id,
-          accessToken: response.session_token,
-          accessJWT: response.session_jwt,
-        });
-        setUserState(DEFAULT_LOCAL_STORAGE_KEY_FOR_USER_STATE, {
-          email: formData.email,
-          fullName: response.fullName,
-        });
-        navigate(PageRoutes.Home);
-      } else {
-        // Handle error cases with various response types
-        if ("error" in response) {
-          console.error("Error:", response.error);
-        }
-        console.error("Sign-up failed:", response.message);
-      }
-    } catch (error) {
-      // Handle network or other errors
-      console.error("Error signing up:", error);
-    }
-  };
+  //     // Handle successful response
+  //     if (response.success) {
+  //       signInUser({
+  //         email: formData.email,
+  //         user_id: response.user_id,
+  //         sessionToken: response.session_token,
+  //         sessionJwt: response.session_jwt,
+  //       });
+  //       updateAppState({
+  //         isUserLoggedIn: true,
+  //         user_id: response.user_id,
+  //         accessToken: response.session_token,
+  //         accessJWT: response.session_jwt,
+  //       });
+  //       setUserState(DEFAULT_LOCAL_STORAGE_KEY_FOR_USER_STATE, {
+  //         email: formData.email,
+  //         fullName: response.fullName,
+  //       });
+  //       navigate(PageRoutes.Home);
+  //     } else {
+  //       // Handle error cases with various response types
+  //       if ("error" in response) {
+  //         console.error("Error:", response.error);
+  //       }
+  //       console.error("Sign-up failed:", response.message);
+  //     }
+  //   } catch (error) {
+  //     // Handle network or other errors
+  //     console.error("Error signing up:", error);
+  //   }
+  // };
 
   return (
     <PageLayout>
@@ -80,7 +76,7 @@ const SignInPage = () => {
         <Col md="8" lg="6" xl="4">
           <CardSimple title="Sign In" error="Some error">
             <Formik
-              initialValues={formData}
+              initialValues={DEFAULT_LOGIN_VALUES}
               validationSchema={SIGN_IN_VALIDATION_SCHEME}
               onSubmit={(values, { setSubmitting }) => {
                 setTimeout(() => {
