e37f328ead72cd0c64bdf5fef8083276932cd318
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-10-17 01:59:48 +0530

    some change

--- src/pages/project.profile.tsx
+++ src/pages/project.profile.tsx
@@ -1,11 +1,9 @@
 import { useContext, useState } from "react";
-import PageLayout from "../lib/components/app.layout";
 import { AppContext } from "../lib/contexts/appcontext";
 import { getProjectById } from "../lib/utils/project.utils";
-import { Col, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import { PageRoutes, ProjectStatus } from "../lib/constants";
 import { useNavigate } from "react-router-dom";
-import ProjectProfileSwitcher from "../lib/components/profile.project/profile.project.switcher";
 import ProfileProjectLayout from "../lib/components/profile.project/profile.project.layout";
 import ProjectOverview from "../lib/components/profile.project/project.overview";
 
